1000505f2e699cae6d096dfae655fc7d9ffd8e7c
Laryssa Ribeiro <laryssarib@gmail.com>
2022-07-20 16:20:55 -0300

    [FIX] Imports

---
+++ 0x0E-Session_authentication/models/user_session.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Model UserSession"""
+
+from models.base import Base
+import hashlib
+
+
+class UserSession(Base):
+    """Class UserSession"""
+    def __init__(self, *args: list, **kwargs: dict):
+        """Init method"""
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
